eab7be2229d21a624c4a4e3a2e0b4b7f45fe321f
WangWenBin2017 <30495356+WangWenBin2017@users.noreply.github.com>
2021-02-13 19:55:42 +0800

    解决NaiveProxy云编译报错

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,9 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# repatch kenso8's naiveproxy patchfiles
+sed -i 's/if (is_linux) {/if (is_linux || is_chromeos) {/' package/feeds/kenzo/naiveproxy/patches/100-build-add-OpenWrt-staging-libraries.patch
+sed -i 's/@@ -746,7 +746,6/@@ -751,7 +751,6/' package/feeds/kenzo/naiveproxy/patches/101-build-drop-useless-deps-simd_asm.patch
+
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
